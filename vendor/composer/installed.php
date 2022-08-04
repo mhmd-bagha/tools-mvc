631b118b3ef481b9478e819b56d7c5bcc4f0557a
mohammad ebrahimi bagha <mabrahimibagha@gmail.com>
2022-08-04 20:44:55 +0430

    tools mvc project v2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '350904d73b4126ba712aedc1de7c17721997dbe4',
+        'name' => 'vendor_name/tools-mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/tools-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '350904d73b4126ba712aedc1de7c17721997dbe4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
